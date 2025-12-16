170d13049546c38faaf942488e53ddd5747b0d80
Katsiaryna Yakubava <yekaterinayak@gmail.com>
2025-12-16 16:17:16 +0100

    faulty REINFORCE method

--- gymnasium/actor-critic/REINFORCE.py
+++ gymnasium/actor-critic/REINFORCE.py
@@ -1,121 +1,127 @@
 import gymnasium as gym
+import numpy as np
 import torch
 import torch.nn as nn
 import torch.optim as optim
 
-#it plateaus at 200 - it decides that just standing in one place is better (T-T) 
-#takes 1000 episodes to reach the plateau
-
 GAMMA = 0.99
-LR = 0.001
+LEARNING_RATE = 0.01
 
-class PolicyNetwork(nn.Module):
+
+
+
+class NeuralNetwork(nn.Module):
     def __init__(self):
         super().__init__()
-        # we use embedding because the states are descrete
-        self.embedding = nn.Embedding(500, 100) # 500 - number of states
-        self.net = nn.Sequential(
+        self.fc = nn.Sequential(
+            #observation space: Box([-4.8 -inf -0.41887903 -inf], [4.8 inf 0.41887903 inf], (4,), float32)
+            nn.Linear(4, 128), 
             nn.ReLU(),
-            nn.Linear(100, 100),
+            nn.Linear(128, 64),
             nn.ReLU(),
-            nn.Linear(100, 6) # 6 - number of actions
+            nn.Linear(64, 2) #two actions: left, right.
         )
+    
+    def forward(self, input):
+        return self.fc(input)
 
-    def forward(self, state):
-        x = self.embedding(state)
-        return self.net(x)  
+
 
 
 class Agent:
     def __init__(self):
-        env = gym.make("Taxi-v3")
-        self.env = gym.wrappers.TimeLimit(env, 200)
+        self.env = gym.make("CartPole-v1")
+        self.policy = NeuralNetwork()
+        self.optimizer = optim.Adam(self.policy.parameters(), lr=LEARNING_RATE)
 
-        self.policy = PolicyNetwork()
-        self.optimizer = optim.Adam(self.policy.parameters(), lr=LR)
-
-        self.saved_log_probs = []
+        self.softmax = nn.Softmax(dim=1)
+        
+        self.log_probabilities = [] #for calculating the loss
         self.rewards = []
 
 
+
     def select_action(self, state):
-        state_t = torch.tensor([state], dtype=torch.long)
+        state_t = torch.tensor(state, dtype=torch.float32)
+        state_t = state_t.unsqueeze(0)
 
-        logits = self.policy(state_t)
-        probs = torch.softmax(logits, dim=-1)
-        distribution = torch.distributions.Categorical(probs)
-        action = distribution.sample()
-        log_prob = distribution.log_prob(action)
+        prediction = self.policy(state_t)
+        probabilities = self.softmax(prediction)            #for example [0.7, 0.3], so 70% left, 30% right.
+        action = torch.multinomial(probabilities, num_samples=1).item()     #picks the action based on its probability
 
-        self.saved_log_probs.append(log_prob)
+        log_prob = torch.log(probabilities[0, action])
+        self.log_probabilities.append(log_prob)
 
-        return action.item()
-
-
-    def play_episode(self, env=None, no_learning=False):
-        if env is None:
-            env = self.env
-
-        state, _ = env.reset()
-        terminated = truncated = False
-        total_reward = 0
-        if not no_learning:
-            self.saved_log_probs = []
-            self.rewards = []
-
-        while not (terminated or truncated):
-            action = self.select_action(state)
-            next_state, reward, terminated, truncated, info = env.step(action)
-
-            total_reward += reward
-
-            if not no_learning:
-                self.rewards.append(reward)
-
-            state = next_state
-
-        # only updating if we are training (not testing/visualization)
-        if not no_learning:
-            self.update_policy()
-
-        return total_reward
+        return action
+    
 
 
     def update_policy(self):
         returns = []
         G = 0
-        for r in reversed(self.rewards):
+        for r in reversed(self.rewards):    #going backwards
             G = r + GAMMA * G
             returns.append(G)
         returns.reverse()
 
-        # policy gradient loss = -sum(log(pi(a|s)) * G_t)
+        returns = torch.tensor(returns, dtype=torch.float32)
+
         loss = 0
-        for log_prob, G_t in zip(self.saved_log_probs, returns):
-            loss += -log_prob * G_t
+        for i in range(len(self.log_probabilities)):
+            loss += -self.log_probabilities[i] * returns[i]
+        
         self.optimizer.zero_grad()
         loss.backward()
         self.optimizer.step()
 
+        self.log_probabilities.clear()
+        self.rewards.clear()
+
+        
+    
+    def play_episode(self, env=None):
+        state, _ = self.env.reset()
+        terminated, truncated = False, False
+        total_reward = 0.0
+
+        while not (terminated or truncated):
+            action = self.select_action(state)
+            next_state, reward, terminated, truncated, _ = self.env.step(action)
+            total_reward += reward
+            self.rewards.append(reward)
+
+            state = next_state
+        
+        self.update_policy()
+        return total_reward
+
+
 
 
 if __name__ == "__main__":
-    env = gym.make("Taxi-v3", render_mode="human")
-    env = gym.wrappers.TimeLimit(env, 200)
+    env = gym.make("CartPole-v1")
     agent = Agent()
 
-    for i in range(50):
-        # train 100 episodes
-        for _ in range(100):
+    for i in range(1, 100):
+        # train 10 episodes per batch
+        for _ in range(10):
             agent.play_episode()
 
-        # evaluate for 10 episodes
+        # evaluate for 5 episodes
         reward = 0
-        for _ in range(10):
-            reward += agent.play_episode(env=None, no_learning=True)
+        for _ in range(5):
+            reward += agent.play_episode()
+        reward /= 5
 
-        reward /= 10
-        print(f"Average reward after {i*100} episodes: {reward}")
+        print(f"Average reward after {i*10} episodes: {reward}")
 
-    # for visualization in the end:
-    agent.play_episode(env, no_learning=True)
+        if reward ==500.0:
+            agent.play_episode(env= env)
+    
+    # final visualization
+    agent.play_episode(env= env)
+
+
+
+
+
